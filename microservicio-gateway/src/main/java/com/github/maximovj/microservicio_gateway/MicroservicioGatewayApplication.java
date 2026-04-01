007b77b543cb650cd1e0a0e58c55930e55f789f5
Víctor J. <maximovj@outlook.com>
2026-04-01 04:03:03 -0600

    * Se agrega Gateway

---
+++ microservicio-gateway/src/main/java/com/github/maximovj/microservicio_gateway/MicroservicioGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.github.maximovj.microservicio_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+// TODO : Microservicio | API | DISCOVERY CLIENT
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MicroservicioGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioGatewayApplication.class, args);
+	}
+
+}
